bd7e846e558c2989b7d4d8494ed10096fb73101d
Arjun Pawar <Arjun.Pawar@aurionpro.com>
2024-07-17 00:31:40 +0530

    Add project files.

---
+++ Models/Calculator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace BMICalculator.Models
+{
+    internal class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Height { get; set; } = 5;
+        public double Weight { get; set; } = 50;
+
+        public Person(int id, string name, int age)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+        }
+
+        
+        public Person(int id, string name, int age, double height, double weight) : this(id, name, age)
+
+        {
+            Height = height;
+            Weight = weight;
+        }
+
+       
+        public double CalculateBMI()
+        {
+            return Weight / (Height * Height);
+        }
+
+        
+        public string GetBodyType()
+        {
+            double bmi = CalculateBMI();
+            if (bmi < 19)
+            {
+                return "Underweight";
+            }
+            else if (bmi >= 19 && bmi < 24)
+            {
+                return "Normal";
+            }
+            else if (bmi >= 25 && bmi < 30)
+            {
+                return "Overweight";
+            }
+            else
+            {
+                return "Obese";
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using BMICalculator.Models;
+
+namespace BMICalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Person person1 = new Person(101, "Allen", 24, 1.75, 70);
+            Console.WriteLine($"The name of the person is {person1.Name}\n" +
+                              $"The age is {person1.Age}\n" +
+                              $"The BMI is {person1.CalculateBMI()}\n" +
+                              $"The person is {person1.GetBodyType()}");
+
+            
+            Person person2 = new Person(102, "Bob", 30);
+            Console.WriteLine($"The name of the person is {person2.Name}\n" +
+                              $"The age is {person2.Age}\n" +
+                              $"The BMI is {person2.CalculateBMI()}\n" +
+                              $"The person is {person2.GetBodyType()}");
+        }
+    }
+}
